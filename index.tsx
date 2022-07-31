7c6966a0c4e49c11b9da8194462ec4a9bdd1bd5a
dbautistav <dbautistav@gmail.com>
2022-07-30 22:31:22 -0500

    More syncing

--- index.tsx
+++ index.tsx
@@ -1,14 +1,12 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
+const root = createRoot(rootElement!);
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
